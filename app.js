da653171318394db57ef981b0718f1ebcc6f5497
Alkyys <samuelvgnl@gmail.com>
2019-10-12 19:50:44 +0200

    ◼ add cors error

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express();
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(express.json());
+
+//gestion de cors errors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  if (req.methode === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATH, DELETE, GET')
+    return res.status(200).json({})
+  }
+  next()
+})
 
 // my routes
 const colorsRoutes = require('./routes/colors')
