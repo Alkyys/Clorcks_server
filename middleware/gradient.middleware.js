820660d2d866e9217936570aaae5c2b604619dd5
Samuel <samuelvgnl@gmail.com>
2019-11-16 01:58:38 +0100

    Alkyys
    ◾ ajout middleware validation palette

--- middleware/gradient.middleware.js
+++ middleware/gradient.middleware.js
@@ -6,9 +6,9 @@ export const validation = [
     .isMongoId()
     .matches(/^(?=[a-f\d]{24}$)(\d+[a-f]|[a-f]+\d)$/).withMessage(`ObjectId invalide`),
   check('stops')
-    .isEmpty().withMessage(`Stops Manquant`)
-    .matches(/^\[((\{"color":"([a-f0-9)]{24})","position":"([0-9]{1}|[0-9]{2}|100)"\})(|,)){2,24}\]$/).withMessage(`couleur bleu invalide`),
+    .isEmpty().withMessage(`Stops manquant`)
+    .matches(/^\[((\{"color":"([a-f0-9)]{24})","position":"([0-9]{1}|[0-9]{2}|100)"\})(|,)){2,24}\]$/).withMessage(`Stops invalide`),
   check('label')
-    .isEmpty().withMessage(`c'est vide`)
-    .matches(/^([A-Za-z0-9 '-]{3,25})$/).withMessage(`couleur alpha invalide`)
+    .isEmpty().withMessage(`label manquant`)
+    .matches(/^([A-Za-z0-9 '-]{3,25})$/).withMessage(`label invalide`)
 ]
---
+++ middleware/palette.middleware.js
@@ -0,0 +1,14 @@
+import { check } from 'express-validator'
+
+export const validation = [
+  check('user_id')
+    .isEmpty().withMessage(`user_id manquant`)
+    .isMongoId()
+    .matches(/^(?=[a-f\d]{24}$)(\d+[a-f]|[a-f]+\d)$/).withMessage(`ObjectId invalide`),
+  check('color_id')
+    .isEmpty().withMessage(`color_id manquant`)
+    .matches(/^\[("([a-f\d]{24})"(|,)){2,5}\]$/).withMessage(`couleur bleu invalide`),
+  check('label')
+    .isEmpty().withMessage(`label manquant`)
+    .matches(/^([A-Za-z0-9 '-]{3,25})$/).withMessage(`couleur alpha invalide`)
+]
