03c084c6607eb9b45a7535989c169148d6250c21
Samuel <samuelvgnl@gmail.com>
2019-11-20 21:58:11 +0100

    Change process.env.port

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 import app from './app.js';
 import webpack from 'webpack';
 
-const port = process.env.NODE_ENV === "production" ?
-  process.env.PORT || 80 :
-  8000;
+// const port = process.env.NODE_ENV === "production" ?
+//   process.env.PORT || 80 :
+//   8000;
+  const port = process.env.PORT || 3000;
 
 app.listen(port, () =>  console.log(`Clocks listening on port ${port}! 🚀`));
