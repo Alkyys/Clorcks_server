b3ecf298dd6a4bbaf73d7281be19d71210fd3ea1
Samuel <samuelvgnl@gmail.com>
2019-11-20 01:02:45 +0100

    implementation aes256 hash
    update signup and login

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,6 @@
 import User from './../models/User'
-import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import crypto from 'crypto'
 
 export function getAll (req, res) {
   User.find().limit(200).exec()
@@ -38,71 +38,77 @@ export function get (req, res) {
 }
 
 export function signup (req, res) {
-  User.find({ email: req.body.email }).exec()
+  User.findOne({ email: req.body.email }).exec()
     .then(user => {
-      if (user.length >= 1) {
+      console.log("TCL: signup -> user", user)
+      if (user) { // on regardde si on a recu quelque chose
         return res.status(409).json({
           message: "mail existant"
         })
       } else {
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          if (err) {
-            return res.status(500).json({
+        let cipher = crypto.createCipheriv('aes-256-cbc', process.env.KEY_CRYPTO, process.env.IV);
+        console.log("TCL: signup -> cipher", cipher)
+
+        let encrypted = cipher.update(req.body.password, 'utf-8', 'hex');
+        encrypted += cipher.final('hex');
+        console.log("TCL: signup -> encrypted", encrypted)
+
+        const user = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: encrypted
+        })
+        user.save()
+          .then(result => {
+            res.status(201).json({
+              result
+            })
+          })
+          .catch(err => {
+            res.status(500).json({
               error: err
             })
-          } else {
-            const user = new User({
-              name: req.body.name,
-              email: req.body.email,
-              password: hash
-            })
-            user.save()
-              .then(result => {
-                res.status(201).json({
-                  result
-                })
-              })
-              .catch(err => {
-                res.status(500).json({
-                  error: err
-                })
-              })
-          }
-        })
+          })
+
       }
     })
 
 }
 
 export function login (req, res) {
-  User.find({ email: req.body.email })
+  User.findOne({ email: req.body.email })
     .exec()
     .then(user => {
-      if (user.length < 1) {
+      console.log("TCL: login -> user", user)
+      if (!user) { // mail inexistant
         return res.status(404).json({
           message: 'Auth failed'
         })
       }
-      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-        if (result) {
-          const token = jwt.sign({
-            email: user[0].email,
-            user_id: user[0]._id,
-            name: user[0].name
-          }, process.env.JWT_SECRET, {
-            expiresIn: "1h"
-          })
-          return res.status(200).json({
-            message: "Auth successful",
-            token: token
-          })
-        } else {
-          return res.status(404).json({
-            message: 'Auth failed'
-          })
-        }
-      })
 
+      let decipher = crypto.createDecipheriv('aes-256-cbc', process.env.KEY_CRYPTO, process.env.IV);
+
+      let decrypted = decipher.update(user.password, 'hex', 'utf-8');
+      decrypted += decipher.final('utf-8');
+
+      if (req.body.password === decrypted) {
+
+        const token = jwt.sign({
+          email: user.email,
+          user_id: user._id,
+          name: user.name
+        }, process.env.JWT_SECRET, {
+          expiresIn: "1h"
+        })
+        return res.status(200).json({
+          message: "Auth successful",
+          token: token
+        })
+      } else {
+        return res.status(404).json({
+          message: 'Auth failed'
+        })
+      }
     }).catch(err => {
       res.status(401).json({
         message: "Auth faild",
--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 import app from './app.js';
+import crypto from 'crypto'
 
 const port = process.env.NODE_ENV === "production" ?
   process.env.PORT || 80 :
   8000;
 
-app.listen(port, () =>
+app.listen(port, () => {
   console.log(`Clocks listening on port ${port}! 🚀`)
-);
+
+  // const secret_message = "123"
+  // console.log("TCL: secret_message", secret_message)
+
+  // let cipher = crypto.createCipheriv('aes-256-cbc', process.env.KEY_CRYPTO, process.env.IV);
+  // let encrypted = cipher.update(secret_message, 'utf-8', 'hex');
+  // encrypted += cipher.final('hex');
+
+  // console.log('encrypted: ' + encrypted)
+
+  // const encrypted = '53a9a0a2e9bf24c2866f5674d3b3e8bd'
+
+  // let decipher = crypto.createDecipheriv('aes-256-cbc', process.env.KEY_CRYPTO, process.env.IV);
+  // let decrypted = decipher.update(encrypted, 'hex', 'utf-8');
+  // decrypted += decipher.final('utf-8');
+
+  // console.log('decrypted: ' + decrypted)
+
+});
