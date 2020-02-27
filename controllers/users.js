247db34a6afdd2b4c975d6f9af08c11abaff64d4
Samuel <samuelvgnl@gmail.com>
2020-02-27 02:06:46 +0100

    Update users.js
    fix issue 2

--- controllers/users.js
+++ controllers/users.js
@@ -37,42 +37,48 @@ export function get (req, res) {
 export function signup (req, res) {
   User.findOne({ email: req.body.email }).exec()
     .then(user => {
-      if (user) { // on regardde si on a recu quelque chose
+      if (user) { // on regardde si on a recu quelque chose de la bd
         return res.status(409).json({
           message: "mail existant"
         })
       } else {
+
+        // on cree le mot de passe encrypted
         let cipher = crypto.createCipheriv('aes-256-cbc', process.env.KEY_CRYPTO, process.env.IV);
         let encrypted = cipher.update(req.body.password, 'utf-8', 'hex');
         encrypted += cipher.final('hex');
 
+        // on cree notre user
         const user = new User({
           name: req.body.name,
           email: req.body.email,
           password: encrypted
         })
+
         user.save()
-        .then(result => {
-          const workspace = new Workspace({
-            user_id: result._id, 
-            name: "main"
-          })
-          try {
-            workspace.save() // on cree le premier workspace de user
-          } catch (err) {
-             res.status(500).json({
+          .then(result => {
+            // on cree notre premier workspace avec le _id qu'on a recu 
+            // on le nommera main
+            const workspace = new Workspace({
+              user_id: result._id,
+              name: "main"
+            })
+            try {
+              workspace.save()
+            } catch (err) {
+              res.status(500).json({
                 error: err
               })
-          }
-          res.status(201).json({
-            result
+            }
+            res.status(201).json({
+              result
+            })
           })
-        })
-        .catch(err => {
-          res.status(500).json({
-            error: err
+          .catch(err => {
+            res.status(500).json({
+              error: err
+            })
           })
-        })
       }
     })
 }
@@ -88,7 +94,6 @@ export function login (req, res) {
       }
 
       let decipher = crypto.createDecipheriv('aes-256-cbc', process.env.KEY_CRYPTO, process.env.IV);
-
       let decrypted = decipher.update(user.password, 'hex', 'utf-8');
       decrypted += decipher.final('utf-8');
 
@@ -101,12 +106,15 @@ export function login (req, res) {
         }, process.env.JWT_SECRET, {
           expiresIn: "1h"
         })
+
+// TODO: on envoi le workpace de l'utilisateur
+
         return res.status(200).json({
           message: "Auth successful",
           token: token
         })
       } else {
-        return res.status(404).json({
+        return res.status(401).json({
           message: 'Auth failed'
         })
       }
